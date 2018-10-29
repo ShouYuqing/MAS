8f85ea506b0a8aeae4b5f1b75e1ff1125f2dd688
yuqing <573380218@qq.com>
2018-10-29 19:20:13 -0400

    Update src/SAS_test.py

--- src/SAS_test.py
+++ src/SAS_test.py
@@ -28,55 +28,46 @@ import datagenerators
 
 
 def test(iter_num, gpu_id, vol_size=(160,192,224), nf_enc=[16,32,32,32], nf_dec=[32,32,32,32,32,16,16,3]):
-	"""
-	test
+ gpu = '/gpu:' + str(gpu_id)
 
-	nf_enc and nf_dec
-	#nf_dec = [32,32,32,32,32,16,16,3]
-    # This needs to be changed. Ideally, we could just call load_model, and we wont have to
-    # specify the # of channels here, but the load_model is not working with the custom loss...
-    """
+    # Anatomical labels we want to evaluate
+ labels = sio.loadmat('../data/labels.mat')['labels'][0]
 
-	gpu = '/gpu:' + str(gpu_id)
+ atlas = np.load('../data/atlas_norm.npz')
+ atlas_vol = atlas['vol']
+ atlas_seg = atlas['seg']
+ atlas_vol = np.reshape(atlas_vol, (1,)+atlas_vol.shape+(1,))
 
-	# Anatomical labels we want to evaluate
-	labels = sio.loadmat('../data/labels.mat')['labels'][0]
+ config = tf.ConfigProto()
+ config.gpu_options.allow_growth = True
+ config.allow_soft_placement = True
+ set_session(tf.Session(config=config))
 
-	atlas = np.load('../data/atlas_norm.npz')
-	atlas_vol = atlas['vol']
-	atlas_seg = atlas['seg']
-	atlas_vol = np.reshape(atlas_vol, (1,)+atlas_vol.shape+(1,))
+ # load weights of model
+ with tf.device(gpu):
+    net = networks.unet(vol_size, nf_enc, nf_dec)
+    net.load_weights('/home/ys895/SAS_Models/'+str(iter_num)+'.h5')
+    #net.load_weights('../models/' + model_name + '/' + str(iter_num) + '.h5')
 
-	config = tf.ConfigProto()
-	config.gpu_options.allow_growth = True
-	config.allow_soft_placement = True
-	set_session(tf.Session(config=config))
+ xx = np.arange(vol_size[1])
+ yy = np.arange(vol_size[0])
+ zz = np.arange(vol_size[2])
+ grid = np.rollaxis(np.array(np.meshgrid(xx, yy, zz)), 0, 4)
 
-	# load weights of model
-	with tf.device(gpu):
-         net = networks.unet(vol_size, nf_enc, nf_dec)
-         net.load_weights('/home/ys895/SAS_Models/'+str(iter_num)+'.h5')
-         #net.load_weights('../models/' + model_name + '/' + str(iter_num) + '.h5')
+ X_vol, X_seg = datagenerators.load_example_by_name('../data/test_vol.npz', '../data/test_seg.npz')
 
-	xx = np.arange(vol_size[1])
-	yy = np.arange(vol_size[0])
-	zz = np.arange(vol_size[2])
-	grid = np.rollaxis(np.array(np.meshgrid(xx, yy, zz)), 0, 4)
-
-	X_vol, X_seg = datagenerators.load_example_by_name('../data/test_vol.npz', '../data/test_seg.npz')
-
-    # change the direction of the atlas data and volume data
-	with tf.device(gpu):
-		pred = net.predict([atlas_vol, X_vol])
+ # change the direction of the atlas data and volume data
+ with tf.device(gpu):
+	pred = net.predict([atlas_vol, X_vol])
 
 	# Warp segments with flow
-	flow = pred[1][0, :, :, :, :]
-	sample = flow+grid
-	sample = np.stack((sample[:, :, :, 1], sample[:, :, :, 0], sample[:, :, :, 2]), 3)
-	warp_seg = interpn((yy, xx, zz), X_seg[0, :, :, :, 0], sample, method='nearest', bounds_error=False, fill_value=0)
+ flow = pred[1][0, :, :, :, :]
+ sample = flow+grid
+ sample = np.stack((sample[:, :, :, 1], sample[:, :, :, 0], sample[:, :, :, 2]), 3)
+ warp_seg = interpn((yy, xx, zz), X_seg[0, :, :, :, 0], sample, method='nearest', bounds_error=False, fill_value=0)
 
-	vals, _ = dice(warp_seg, atlas_seg, labels=labels, nargout=2)
-	print(np.mean(vals), np.std(vals))
+ vals, _ = dice(warp_seg, atlas_seg, labels=labels, nargout=2)
+ print(np.mean(vals), np.std(vals))
 
 
 if __name__ == "__main__":
