5b888283d07bf3be5484a985332811d0ecad9c5c
yuqing <573380218@qq.com>
2018-10-30 18:43:56 -0400

    Update src/demo.py

--- src/demo.py
+++ src/demo.py
@@ -33,7 +33,7 @@ X_seg_slice = X_seg[0, :, :, :, 0 ]
 #print(X_seg_slice.shape)
 X_seg_slice = X_seg_slice.reshape((X_seg_slice.shape[1],X_seg_slice.shape[0],X_seg_slice.shape[2]))
 #X_seg_slice = X_seg_slice.reshape([X_seg_slice.shape[0],X_seg_slice.shape[1],X_seg_slice.shape[2]])
-print(X_seg_slice.shape)
+X_seg_slice = [X_seg_slice]
 fig,axs = nu.plot.slices(X_seg_slice)
 #fig.set_size_inches(width, rows/cols*width)
 #plt.tight_layout()
